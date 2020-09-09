40c8a5084f1fa7ce3f897ff266c452cd1eafbc79
CHIHAB09 <web2020.chihab@gmail.com>
2020-09-09 16:37:56 +0200

    2em test connectDB

--- connectDB.php
+++ connectDB.php
@@ -1,6 +1,6 @@
 <?php
 //connectDB
-$db=@mysqli_connect(DB_HOST,DB_LOGIN,DB_PASS,DB_NAME,DB_CHARSET);
+$db=connectDB(DB_HOST,DB_LOGIN,DB_PASS,DB_NAME,DB_CHARSET);
 mysqli_set_charset($db,"UTF8");
 
 ?>
